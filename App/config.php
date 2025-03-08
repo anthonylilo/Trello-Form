63da7324a2b65900aeeb1fb7b64d954762cec9c5
Anthonylilo <ammen0203@gmail.com>
2025-03-08 13:22:53 -0300

    Fix: Config file give the credentials

--- App/config.php
+++ App/config.php
@@ -2,13 +2,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+$dotenv->safeLoad();
 
 return [
     'trello' => [
-        'key' => getenv('TRELLO_KEY'),
-        'token' => getenv('TRELLO_TOKEN'),
-        'idList' => getenv('TRELLO_ID_LIST')
+        'key' => $_ENV['TRELLO_KEY'] ?? getenv('TRELLO_KEY') ?? null,
+        'token' => $_ENV['TRELLO_TOKEN'] ?? getenv('TRELLO_TOKEN') ?? null,
+        'idList' => $_ENV['TRELLO_ID_LIST'] ?? getenv('TRELLO_ID_LIST') ?? null
     ],
-    'log_file' => __DIR__ . '/../' . getenv('LOG_FILE_PATH')
+    'log_file' => __DIR__ . '/../logs/error.log'
 ];
